4f40549084ff1d1bf59e7746d4546a92cb029685
Daniel Chung <pikachungg@gmail.com>
2023-05-25 17:14:05 -0400

    fixing

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import os
 from pathlib import Path
 
 def main(): 
+	#Get tomorrow's date
 	date = (datetime.today() + timedelta(1)).strftime('%m-%d-%Y')
 
+	#Check if file exists or not
 	path = 'standups/{0}.txt'.format(date)
 	mode = 'a' if os.path.exists(path) else 'w'
 
@@ -12,15 +14,15 @@ def main():
 	filename = Path(path)
 
 	with file as f:  
-
+		#If the file doesn't exist add the header
 		if mode == "w":
-			f.write("**Yesterday**\n")
+			f.write("- Yesterday\n")
 
+		#Ask prompt and write users answers.
 		user_action = None
 		while (user_action != ""):
 			user_action = input("What task did you accomplish? (enter to quit): ")
 			f.write("{0}\n".format(user_action))
-		f.write("**Today**\n")
 
 	f.close()
 
