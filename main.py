a8c40c3b78d26270075c65a49c30496bb24d867d
Daniel Chung <pikachungg@gmail.com>
2023-05-25 17:08:43 -0400

    finished

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello world")
+from datetime import datetime, timedelta
+import os
+from pathlib import Path
+
+def main(): 
+	date = (datetime.today() + timedelta(1)).strftime('%m-%d-%Y')
+
+	path = 'standups/{0}.txt'.format(date)
+	mode = 'a' if os.path.exists(path) else 'w'
+
+	file = open(path, mode)
+	filename = Path(path)
+
+	with file as f:  
+
+		if mode == "w":
+			f.write("**Yesterday**\n")
+
+		user_action = None
+		while (user_action != ""):
+			user_action = input("What task did you accomplish? (enter to quit): ")
+			f.write("{0}\n".format(user_action))
+		f.write("**Today**\n")
+
+	f.close()
+
+main()
